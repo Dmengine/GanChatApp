7d89715788da7509cd31ca05ebc5eaee9553249e
Olaweb3493 <46165459+Dmengine@users.noreply.github.com>
2025-08-03 11:17:29 +0100

    global state changed

--- src/api/global.ts
+++ src/api/global.ts
@@ -1,3 +1,3 @@
-// export const SERVER_URL = 'https://api-chatapp.onrender.com';
+export const SERVER_URL = 'https://api-chatapp.onrender.com';
 
-export const SERVER_URL = 'http://localhost:5001';
+// export const SERVER_URL = 'http://localhost:5001';
